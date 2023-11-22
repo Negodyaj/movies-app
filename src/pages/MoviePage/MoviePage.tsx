905fbee57422729f624f1fb5345704dd6f06030d
Anton Efremenkov <aefremenkov@ptsecurity.com>
2023-11-22 18:23:42 +0300

    a bit more markup added

--- src/pages/MoviePage/MoviePage.tsx
+++ src/pages/MoviePage/MoviePage.tsx
@@ -3,5 +3,9 @@ import "./MoviePage.scss";
 import React from "react";
 
 export const MoviePage = () => {
-  return <div>MoviePage</div>;
+  return (
+    <div className="movie-page">
+      <h1>MoviePage</h1>
+    </div>
+  );
 };
